e7bf4c2529778cd5ff9a04952cba9f8dedbe9578
Gabrielli <gabriellisilva1400@gmail.com>
2023-08-24 10:13:11 -0300

    primeiro commit

---
+++ exercicios/ex010/acoes.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida')
+
+function acao1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão'
+}
+
+function acao2() {
+    resp.innerHTML += '<p>Clicou no segundo botão'
+}
+
+function acao3() {
+    resp.innerHTML += '<p>Clicou no terceiro botão'
+}
+
+function acao4() {
+    resp.innerHTML += '<p>Clicou no quarto botão'
+}
